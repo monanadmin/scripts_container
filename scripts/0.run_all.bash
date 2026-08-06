18a150ce3a37cc1b31973e387e2716d7431c52ed
Sylvio Neto <neto.sylvio@gmail.com>
2026-08-06 13:51:54 -0300

    updatint script with resolutions variable options
    Added comments to clarify the RES variable options.

--- scripts/0.run_all.bash
+++ scripts/0.run_all.bash
@@ -43,7 +43,7 @@ github_link="https://github.com/monanadmin/MONAN-Model.git"
 monan_branch=1.4.4
 convertmpas_branch=1.2.0
 EXP=GFS
-RES=1024002
+RES=1024002        #Options: 40962=120km;163842=60km;655362=30Km;1024002=24km;2621442=15Km;5898242=10Km
 YYYYMMDDHHi=2025061000
 FCST=06
 #----------------------------------------------------------------------
